df7f904625cffe6ad640452f1e380667973f9f4e
ville <ville.gillstrom@gmail.com>
2019-03-26 16:20:18 +0100

    Changes ifelse to switch

--- Step1/Step1_1A/miserable.cpp
+++ Step1/Step1_1A/miserable.cpp
@@ -3,84 +3,105 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-using namespace std;
+#include "miserable.h"
+
+
+
+using std::cout;
+using std::cin;
+using std::ifstream;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::endl;
 
 int main()
 {
 	bool isRunning = true; /* Determines when to exit main loop (terminates application) */
-	double t;
+	double temperature;
 	double s;
 	double m;
-	char b;
+	char choice;
 
 	cout << "\n\nTemperature Statistics\n----------------------\n\nReading logged values for processing and presentation...\n\nPress Enter for menu: ";
 	cin.get();
 
 	while (isRunning)
 	{
-		system("cls");
+		system("cls"); /* Clear screen */
 		cout << "\n\nMENU\n----\n\n1. Display temperature values\n2. View maximum and minimum temperatures\n3. View average temperature\n4. Quit\n\nMake your choice: ";
-		cin.get(b);
+
+		cin.get(choice);
 		cin.get(); /* Press enter to continue prompt */
 
-		if (b == '1')
-		{
-			cout << "\nDisplaying the latest 24 temperature values:\n\n";
-			ifstream fil("templog.txt");
-			for (int i = 0; i < 24; i++)
-			{
-				if (i % 6 == 0)
-					cout << endl;
-				fil >> t;
-				cout << fixed << setprecision(2) << setw(8) << t;
-			}
-			fil.close();
-		}
-
-		else if (b == '2')
-		{
-			cout << "\nCalculating the maximum and minimum temperature...\n";
-			double max = 0, min = 0;
-			ifstream fil("templog.txt");
-			fil >> t;
-			max = min = t;
-			for (int i = 1; i < 24; i++)
-			{
-				fil >> t;
-				if (t > max)
-					max = t;
-				if (t < min)
-					min = t;
-			}
-			fil.close();
-			cout << "\nMaximum temperature: " << fixed << setprecision(2) << max << " degrees Celcius\n";
-			cout << "\nMinimum temperature: " << min << " degrees Celcius\n";
-		}
-
-		else if (b == '3')
-		{
-			cout << "\nCalculating average temperature...\n";
-			s = 0.0;
-			ifstream fil("templog.txt");
-			for (int i = 0; i < 24; i++)
-			{
-				fil >> t;
-				s += t;
-			}
-			fil.close();
-			m = s / 24;
-			cout << "\nAverage temperature: ";
-			cout << fixed << setprecision(2) << m << " degrees Celcius\n";
-		}
-
-		else
-		{
+		switch (choice) {
+		case '1': 
+			displayTemperature(temperature);
+			break;       
+		case '2': 
+			displayTemperatureMinMax(temperature);
+			break;
+		case '3':
+			displayAvgTemperature(s, temperature, m);
+			break;
+		default:
 			isRunning = 0;
 			cout << "\n\nTerminating the program.";
+			break;
 		}
 		cout << "\n\nPress Enter to continue:";
 		cin.get();
 	}
 	return 0;
+}
+
+void displayAvgTemperature(double &s, double &temperature, double &m)
+{
+	cout << "\nCalculating average temperature...\n";
+	s = 0.0;
+	ifstream fil("templog.txt");
+	for (int i = 0; i < 24; i++)
+	{
+		fil >> temperature;
+		s += temperature;
+	}
+	fil.close();
+	m = s / 24;
+	cout << "\nAverage temperature: ";
+	cout << fixed << setprecision(2) << m << " degrees Celcius\n";
+}
+
+void displayTemperatureMinMax(double &temperature)
+{
+	cout << "\nCalculating the maximum and minimum temperature...\n";
+	double max = 0, min = 0;
+	ifstream fil("templog.txt");
+	fil >> temperature;
+	max = min = temperature;
+	for (int i = 1; i < 24; i++)
+	{
+		fil >> temperature;
+		if (temperature > max)
+			max = temperature;
+		if (temperature < min)
+			min = temperature;
+	}
+	fil.close();
+	cout << "\nMaximum temperature: " << fixed << setprecision(2) << max << " degrees Celcius\n";
+	cout << "\nMinimum temperature: " << min << " degrees Celcius\n";
+}
+
+void displayTemperature(double &t)
+{
+	cout << "\nDisplaying the latest 24 temperature values:\n\n";
+	ifstream fil("templog.txt");
+	for (int i = 0; i < 24; i++)
+	{
+		if (i % 6 == 0)
+			cout << endl;
+		fil >> t;
+		cout << fixed << setprecision(2) << setw(8) << t;
+	}
+	fil.close();
 }
 
---
+++ Step1/Step1_1A/miserable.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void displayTemperature(double &t);
+
+void displayTemperatureMinMax(double &temperature);
+
+void displayAvgTemperature(double &s, double &temperature, double &m);
